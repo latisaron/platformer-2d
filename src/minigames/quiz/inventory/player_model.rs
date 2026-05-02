1eb2a2d053c4f7cda29a45989036ea2b841dd14d
latisaron <latis.aron@gmail.com>
2026-05-02 22:28:23 +0300

    Relative review button size nd shit

---
+++ src/minigames/quiz/inventory/player_model.rs
@@ -0,0 +1,11 @@
+use bevy::{prelude::*};
+
+#[derive(Component)]
+pub struct PlayerModel;
+
+pub fn setup_player_model(
+    mut commands: Commands,
+    asset_server: Ref<AssetServer>,
+) {
+    
+}
--- src/minigames/quiz/request_review_button.rs
+++ src/minigames/quiz/request_review_button.rs
@@ -2,8 +2,8 @@ use bevy::{prelude::*, window::PrimaryWindow};
 
 use crate::minigames::quiz::QuizGameState;
 
-const IMAGE_WIDTH: f32 = 400.;
-const IMAGE_HEIGHT: f32 = 100.;
+const IMAGE_WIDTH_PERCENTAGE: f32 = 0.15;
+const IMAGE_HEIGH_PERCENTAGE: f32 = 0.1;
 
 const IMAGE_Z_INDEX: f32 = 0.;
 
@@ -20,16 +20,17 @@ pub fn create_request_button(
     asset_server: &mut ResMut<AssetServer>,
     window: &Single<&Window, With<PrimaryWindow>>,
 ) {
+    let window_width = window.width();
     let window_height = window.height();
 
     commands.spawn((
         Sprite {
             image: asset_server.load("quiz_game/request_review_button.png"),
-            custom_size: Some(Vec2::new(IMAGE_WIDTH, IMAGE_HEIGHT)),
+            custom_size: Some(Vec2::new(IMAGE_WIDTH_PERCENTAGE * window_width, IMAGE_HEIGH_PERCENTAGE * window_height)),
             image_mode: SpriteImageMode::Auto,
             ..default()
         },
-        Transform::from_xyz(0., -(window_height / 2. - IMAGE_HEIGHT / 2. - PADDING), IMAGE_Z_INDEX),
+        Transform::from_xyz(0., -(window_height / 2. - IMAGE_HEIGH_PERCENTAGE * window_height / 2. - PADDING), IMAGE_Z_INDEX),
         RequestReviewButton,
         CleanupRequestReviewButton,
     ));
@@ -49,15 +50,17 @@ pub fn handle_request_review_button_interaction(
     button_query: Single<&Transform, With<RequestReviewButton>>,
     mut quiz_game_state: ResMut<NextState<QuizGameState>>,
 ) {
+    let window_width = window.width();
+    let window_height = window.height();
     if keys.just_pressed(MouseButton::Left) {
         if let Some(position) = window.cursor_position() {
             let x =  position[0] - window.width() / 2.0;
             let y = -(position[1] - window.height() / 2.0);
 
-            let button_lwr_x = button_query.translation.x - IMAGE_WIDTH / 2.;
-            let button_upr_x = button_query.translation.x + IMAGE_WIDTH / 2.;
-            let button_lwr_y = button_query.translation.y - IMAGE_HEIGHT / 2.;
-            let button_upr_y = button_query.translation.y + IMAGE_HEIGHT / 2.;
+            let button_lwr_x = button_query.translation.x - window_width * IMAGE_WIDTH_PERCENTAGE / 2.;
+            let button_upr_x = button_query.translation.x + window_width * IMAGE_WIDTH_PERCENTAGE / 2.;
+            let button_lwr_y = button_query.translation.y - window_height * IMAGE_HEIGH_PERCENTAGE / 2.;
+            let button_upr_y = button_query.translation.y + window_height * IMAGE_HEIGH_PERCENTAGE / 2.;
 
             if x >= button_lwr_x && x <= button_upr_x && y >= button_lwr_y && y <= button_upr_y { 
                 quiz_game_state.set(QuizGameState::PasswordPopup);
